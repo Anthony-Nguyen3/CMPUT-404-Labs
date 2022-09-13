599fd1487e35dbd766b6fb55eceaf85f580090d5
Anthony Nguyen <anthony3@ualberta.ca>
2022-09-12 23:19:04 -0600

    Modifed to print python script

--- Lab_1/lab1.py
+++ Lab_1/lab1.py
@@ -1,2 +1,3 @@
 import requests
-print(requests.get("http://www.google.com/"))
+r = requests.get("https://raw.githubusercontent.com/Anthony-Nguyen3/CMPUT-404-Labs/main/Lab_1/lab1.py")
+print(r.text)
